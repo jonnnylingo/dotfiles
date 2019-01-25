49221c3cb121df789c722224cac1f673a4e1a183
Chris Jones <j@ldschurch.org>
2019-01-24 23:37:19 -0700

    Removed the mac app store apps from the brew-apps.sh
    Need to add this using "mas"

--- brew-apps.sh
+++ brew-apps.sh
@@ -7,26 +7,19 @@ brew install homebrew/completions/docker-completion
 brew install homebrew/completions/docker-compose-completion
 
 brew tap caskroom/cask
-# brew cask install 2do  #NoCask
-# brew cask install 1password  #This is version 7.  I need 6.8.9
 brew cask install alfred
 brew cask install atom
-# brew cask install bear #NoCask
 brew cask install bartender
 brew cask install capto
 brew cask install carbon-copy-cloner
-# brew cask install daisy-disk #NoCask
 # brew cask install dashlane
 # brew cask install data-rescue
-# brew cask install dayone #NoCask
 brew cask install docker
 brew cask install dropbox
 # brew cask install evernote
-brew cask install fantastical
 brew cask install figma
 # brew cask install firefox
 brew cask install flash-player  #Adobe Flash
-# brew cask install gif-keyboard #NoCask
 brew cask install gitkraken
 # brew cask install github-desktop
 brew cask install google-chrome
@@ -37,10 +30,8 @@ brew cask install iterm2
 # brew cask install itsycal
 brew cask install java
 # brew cask install kindle
-# brew cask install mindnode #NoCask
 # brew cask install monolingual
 brew cask install paw
-# brew cask install pixelmator #NoCask
 # brew cask install postico
 # brew cask install recordit
 # brew cask install sequel-pro
@@ -48,14 +39,10 @@ brew cask install paw
 # brew cask install sketch-toolbox
 # brew cask install sky-fonts
 # brew cask install skype
-brew cask install slack
 # brew cask install soulver
 # brew cask install spotify
-# brew cask install toothfairy #NoCask
 # brew cask install transmit
 brew cask install tripmode
-# brew cask install twins-mini #NoCask
-# brew cask install typeeto #NoCask
 brew cask install viscosity
 # brew cask install vlc
 brew cask install webstorm
