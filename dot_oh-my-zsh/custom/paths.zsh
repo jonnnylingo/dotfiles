5b1e6aff7ff2bf0ed98a31d24bcb334b5602941a
Chris L Jones <75049534+jonnnylingo2@users.noreply.github.com>
2023-08-07 18:56:23 -0600

    Create paths.zsh
    initial checkin of paths.zsh

---
+++ dot_oh-my-zsh/custom/paths.zsh
@@ -0,0 +1,5 @@
+export NVM_DIR="$HOME/.nvm"
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/amazon-corretto-17.jdk/Contents/Home
