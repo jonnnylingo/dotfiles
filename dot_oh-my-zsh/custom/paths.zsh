74732d055f5ef9ba3c9020f6b2de29e7edaf197d
Chris L Jones <75049534+jonnnylingo2@users.noreply.github.com>
2023-08-07 19:18:31 -0600

    Update paths.zsh

--- dot_oh-my-zsh/custom/paths.zsh
+++ dot_oh-my-zsh/custom/paths.zsh
@@ -3,3 +3,32 @@ export NVM_DIR="$HOME/.nvm"
   [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/amazon-corretto-17.jdk/Contents/Home
+
+gradle-run () {
+    local gradle_cmd
+    export TZ="UTC"
+    gradle_cmd="$(find-upward gradlew)"
+    if [[ ! -f "${gradle_cmd}" ]]
+    then
+        gradle_cmd="gradle"
+    fi
+    "${gradle_cmd}" "$@"
+}
+alias gradle=gradle-run
+ 
+find-upward () {
+    [[ -z "$1" ]] && return 1
+    local name dir
+    name="$1"
+    dir="${PWD}"
+    while [[ "${dir}" != "" ]] && [[ ! -e "${dir}/$name" ]]
+    do
+        dir="${dir%/*}"
+    done
+    if [[ -e "${dir}/$name" ]]
+    then
+        echo "${dir}/$name"
+        return 0
+    fi
+    return 2
+}
